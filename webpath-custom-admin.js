8817c599670cbcc97ccd657fbd0ef01836e0827d
Sebastian Patras <sebastian@webpath.ro>
2024-09-28 22:25:09 +0300

    feat: add in construction text

--- webpath-custom-admin.js
+++ webpath-custom-admin.js
@@ -1,11 +1,16 @@
 jQuery(document).ready(function ($) {
   // Replace the #dashboard-widgets-wrap element with custom HTML
+
   $("#dashboard-widgets-wrap").html(`
-    <div class="custom-dashboard-content">
-        <h2>${dashboardData.username}</h2>
-        <h2>${dashboardData.website}</h2>
-    </div>
-`).append(`
-    <iframe src="https://webpath.ro/clients-dashboard/${dashboardData.website}?username=${dashboardData.username}&key=abc" width="100%" height="800" frameborder="0" allowfullscreen></iframe>
-`);
+    <h2>Buna, ${dashboardData.username}</h2>
+    <p>Momentan, această pagină este în construcție</p>
+    `);
+  //   $("#dashboard-widgets-wrap").html(`
+  //     <div class="custom-dashboard-content">
+  //         <h2>${dashboardData.username}</h2>
+  //         <h2>${dashboardData.website}</h2>
+  //     </div>
+  // `).append(`
+  //     <iframe src="https://webpath.ro/clients-dashboard/${dashboardData.website}?username=${dashboardData.username}&key=abc" width="100%" height="800" frameborder="0" allowfullscreen></iframe>
+  // `);
 });
